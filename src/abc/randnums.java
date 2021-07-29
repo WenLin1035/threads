08a97d466549943bb728f58196da31ae5c463c54
Wenjuan Lin <Harold@Harold.myfiosgateway.com>
2021-07-29 18:45:20 -0400

    use producer and consumer

--- src/abc/randnums.java
+++ src/abc/randnums.java
@@ -4,37 +4,70 @@
  * and open the template in the editor.
  */
 package abc;
+import static abc.randnums.ar;
+import java.lang.reflect.Array;
 import java.util.*;
 /**
  *
  * @author Wenjuan Lin
  */
 public class randnums {
+    public static double ar;
     public static void main(String[] args){
-        Printing p1 = new Printing();
-        while(true){
-            new Thread(p1).start();
-            try{
-                Thread.sleep(2000);
-            } catch(Exception e){
-                System.out.println("ER");
-            }
-        }
-        
+        double sum[] = new double[1];
+        ar = Math.random();
+        sum[0] = ar;
+        Printing p1 = new Printing(sum);
+        Printing1 p2 = new Printing1(sum);
+        p1.start();
+        p2.start();
     }
 }
-class Printing implements Runnable{
+
+
+
+class Printing extends Thread{
+    private double sum[];
+    Printing(double thing[]){
+        sum = thing;
+    }
     @Override
     public void run() {
-        synchronized (Printing.class){
-            while(true){
-                System.out.println(Math.random());
-                Printing.class.notify();
-                try{
-                    Printing.class.wait();
-                } catch(Exception e){
-                    System.out.println("ERROR");
+        while(true){
+            synchronized (sum){
+                while(ar != sum[0]){
+                    try{
+                        sum[0] = ar;
+                        sum.wait();
+                    }catch(Exception e){
+                        System.out.println("error");
+                    }
                 }
+                sum[0] = ar;
+                sum.notify();
+            }
+        }
+    }
+}
+
+class Printing1 extends Thread{
+    private double sum[];
+    Printing1(double thing[]){
+        sum = thing;
+    }
+    @Override
+    public void run() {
+        while(true){
+            synchronized (sum){
+                while(ar == sum[0]){
+                    try{
+                        System.out.println(sum[0]);
+                        ar = Math.random();
+                    }catch(Exception e){
+                        System.out.println("error");
+                    }
+                }
+                sum.notify();
             }
         }
     }
