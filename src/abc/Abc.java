7f23b18c254687bd801b39f6baec3d2b18597e57
Wenjuan Lin <Harold@Harold.myfiosgateway.com>
2021-07-30 17:00:32 -0400

    Print ABC loop using 3 threads synchronized

--- src/abc/Abc.java
+++ src/abc/Abc.java
@@ -17,18 +17,38 @@ import java.util.logging.Logger;
  *
  * @author Wenjuan Lin
  */
-class Print1 extends Thread {
-    private String name;
+class Print1 implements Runnable {
+    public int max=12;
+    static int num = 0;
+    static String letter="A";
+    String name = new String();
     Print1(String name){
         this.name = name;
     }
     @Override
     public void run(){
-        System.out.print(name);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            System.out.println("ERROR");
+        //amount of letters to be printed
+        while(num <= max){
+            synchronized (Abc.lock){
+                //wait if letter and thread parameter not same
+                while(!letter.equals(name)){
+                    try{
+                        Abc.lock.wait();
+                    }catch(Exception e){
+                        System.out.println("ERROR");
+                    }
+                }
+                System.out.print(this.name);
+                if(letter.equals("A")){
+                    letter = "B";
+                }else if(letter.equals("B")){
+                    letter = "C";
+                }else if(letter.equals("C")){
+                    letter = "A";
+                }
+                num++;
+                Abc.lock.notifyAll();
+            }
         }
     }
 }
@@ -38,29 +58,15 @@ public class Abc {
     /**
      * @param args the command line arguments
      */
+    public static Object lock = new Object();
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        int k = 1;
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
-        for(int i=0;i<12;i++){
-            switch(k){
-                case 1:
-                    Print1 p1 = new Print1("A");
-                    Future<String> future = (Future<String>) executor.submit(p1);
-                    future.get();
-                    k++;
-                case 2:
-                    p1 = new Print1("B");
-                    future = (Future<String>) executor.submit(p1);
-                    future.get();
-                    k++;
-                case 3:
-                    p1 = new Print1("C");
-                    future = (Future<String>) executor.submit(p1);
-                    future.get();
-                    k=1;
-            }
-        }
-        executor.shutdown();
+        Print1 p1 = new Print1("A");
+        Print1 p2 = new Print1("B");
+        Print1 p3 = new Print1("C");
+        new Thread(p1).start();
+        new Thread(p2).start();
+        new Thread(p3).start();
+        
     }
     
 }
